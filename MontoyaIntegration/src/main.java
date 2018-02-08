985e1ed23b2ba5d0c44fa8857e769646cadee3a0
dmontoya7434 <dmontoya7434@L438R2S6.STUDENT.ad.fgcu.edu>
2018-02-08 18:34:11 -0500

    starting main class

--- MontoyaIntegration/src/main.java
+++ MontoyaIntegration/src/main.java
@@ -1,4 +1,23 @@
 
+// Daniel Montoya
+// This integration project is on skills that I learned.
+// 50 lines of code 
+import java.util.Scanner;
+
 public class main {
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Welcome to my integration project!");
+    System.out.println();
+    int num1;
+    System.out.println("Enter a number to determine if its odd or even number");
+    num1 = in.nextInt();
+    if (num1 % 2 == 0)
+      System.out.println("The number entered is an even number");
+    else
+      System.out.println("The number entered is odd");
+        
+  }
 
 }
