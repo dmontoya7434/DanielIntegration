b676a8bb25659bf8af33c1732e6f6b6558f2437a
danielmontoya <danielmontoya@10.100.196.57>
2018-04-19 18:11:02 -0400

    spotbug check

--- MontoyaIntegration/src/Main.java
+++ MontoyaIntegration/src/Main.java
@@ -56,12 +56,9 @@ public class Main {
     }
     return max;
   }
-  // public static void divide(int num1, int num2) {
-  // System.out.println(num1 / num2);
-  // }
 
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+//    Scanner in = new Scanner(System.in);
 
     // int num1 = 5;
     // int num2 = 0;
